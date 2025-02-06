3f3883b7717209e518a99f72b5fe6dc11e32b414
buitheanh2802 <theanhbui345@gmail.com>
2025-02-06 22:44:11 +0700

    222

---
+++ React-todolist/webpack.config.js
@@ -0,0 +1,14 @@
+const Webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+/**
+ * @param {*} env 
+ * @param {*} args 
+ * @returns {Webpack.Configuration}
+ */
+module.exports = (env,args) => {
+    return {
+
+    }
+}
